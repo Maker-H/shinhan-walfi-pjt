573c5fef5c589ae11b4e57bb162e6de3d536f862
김민수 <mskim.spring@gmail.com>
2023-10-05 13:31:24 +0900

    [기능] 퀘스트 불러오기

--- backend/src/main/java/com/shinhan/walfi/BackendApplication.java
+++ backend/src/main/java/com/shinhan/walfi/BackendApplication.java
@@ -3,11 +3,14 @@ package com.shinhan.walfi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+
 @EnableScheduling
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class BackendApplication {
 
